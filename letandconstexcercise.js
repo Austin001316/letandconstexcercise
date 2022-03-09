c09ddc6c9105b33405b4146257b2da59b5433e22
Austin001316 <91763031+Austin001316@users.noreply.github.com>
2022-03-09 00:19:21 -0600

    Add files via upload

---
+++ letandconstexcercise.js
@@ -0,0 +1,12 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+const pi = 3.14;
+PI = 42; //error
+
+
+// Quiz //
+// 1. var is applied to the window, hoisted, and can be changed more freely, while let is slightly harder to change, block ({}) based, and not hoisted.
+// 2. Same as above, except const is even harder to change
+// 3. Neither are hoisted, neither can be redclared, let can be reassigned but const cant.
+// 4. Var and function declarations are hoisted and get loaded before everything else on the page
